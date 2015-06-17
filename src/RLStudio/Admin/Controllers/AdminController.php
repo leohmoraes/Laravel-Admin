9df8038930938bd74fc6dd6c8df6d02a540821ca
Leo Sjöberg <leo.sjoberg@gmail.com>
2015-06-17 15:37:29 +0200

    changed back directory structure, felt cleaner

---
+++ src/RLStudio/Admin/Controllers/AdminController.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: leosjoberg
+ * Date: 17/06/15
+ * Time: 13:58
+ */
+
+namespace RLStudio\Admin\Controllers;
+
+
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+
+class AdminController
+{
+
+    public function getDashboard()
+    {
+
+    }
+
+}
